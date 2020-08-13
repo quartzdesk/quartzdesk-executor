929578a9220c894a8337657dba6f3c0d12ee3a61
xmoravej <jan.moravec@quartzdesk.com>
2020-08-13 22:37:50 +0200

    Fixed issue with SSL certificate validation when hitting HTTPS URLs.

--- quartzdesk-executor-core/src/main/java/com/quartzdesk/executor/core/job/UrlInvokerJob.java
+++ quartzdesk-executor-core/src/main/java/com/quartzdesk/executor/core/job/UrlInvokerJob.java
@@ -13,8 +13,10 @@ import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.CredentialsProvider;
 import org.apache.http.client.ResponseHandler;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.conn.ssl.NoopHostnameVerifier;
 import org.apache.http.impl.client.BasicCredentialsProvider;
 import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
 import org.quartz.DisallowConcurrentExecution;
@@ -24,7 +26,15 @@ import org.quartz.JobExecutionException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
 import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.security.GeneralSecurityException;
+import java.security.SecureRandom;
+import java.security.cert.X509Certificate;
 
 /**
  * Quartz job implementation that performs an HTTP POST request to the URL specified in the
@@ -37,9 +47,12 @@ public class UrlInvokerJob
 {
   private static final Logger log = LoggerFactory.getLogger( UrlInvokerJob.class );
 
-  private static final String JDM_KEY_URL = "url";
-  private static final String JDM_KEY_USERNAME = "username";
   private static final String JDM_KEY_PASSWORD = "password";
+
+  private static final String JDM_KEY_URL = "url";
+
+  private static final String JDM_KEY_USERNAME = "username";
+
 
   @Override
   protected void executeJob( final JobExecutionContext context )
@@ -60,8 +73,17 @@ public class UrlInvokerJob
     // password (optional)
     String password = jobDataMap.getString( JDM_KEY_PASSWORD );
 
+    final URI uri;
+    try
+    {
+      uri = new URI( url );
+    }
+    catch ( URISyntaxException e )
+    {
+      throw new JobExecutionException( "Invalid URL syntax.", e );
+    }
 
-    CloseableHttpClient httpClient;
+    HttpClientBuilder httpClientBuilder = HttpClients.custom();
     if ( username != null && password != null )
     {
       // use HTTP basic authentication
@@ -70,20 +92,54 @@ public class UrlInvokerJob
           AuthScope.ANY,
           new UsernamePasswordCredentials( username, password ) );
 
-      httpClient = HttpClients.custom()
-          .setDefaultCredentialsProvider( credsProvider )
-          .build();
+      httpClientBuilder.setDefaultCredentialsProvider( credsProvider );
     }
     else
     {
       // use no HTTP authentication
-      httpClient = HttpClients.custom()
-          .build();
     }
+
+    if ( "https".equals( uri.getScheme() ) )
+    {
+      TrustManager[] trustAllCerts = new TrustManager[] {
+          new X509TrustManager()
+          {
+            public void checkClientTrusted( X509Certificate[] certs, String authType )
+            {
+            }
+
+
+            public void checkServerTrusted( X509Certificate[] certs, String authType )
+            {
+            }
+
+
+            public X509Certificate[] getAcceptedIssuers()
+            {
+              return null;
+            }
+          }
+      };
+
+      try
+      {
+        SSLContext sc = SSLContext.getInstance( "SSL" );
+        sc.init( null, trustAllCerts, new SecureRandom() );
+
+        httpClientBuilder.setSSLHostnameVerifier( NoopHostnameVerifier.INSTANCE )
+            .setSSLContext( sc );
+      }
+      catch ( GeneralSecurityException e )
+      {
+        throw new JobExecutionException( "Error creating SSL context.", e );
+      }
+    }
+
+    CloseableHttpClient httpClient = httpClientBuilder.build();
 
     try
     {
-      HttpPost httpPost = new HttpPost( url );
+      HttpPost httpPost = new HttpPost( uri );
 
       ResponseHandler<String> responseHandler = new ResponseHandler<String>()
       {
@@ -102,25 +158,26 @@ public class UrlInvokerJob
           }
           else
           {
-            throw new ClientProtocolException( "URL: " + url + " returned unexpected response status code: " + status );
+            throw new ClientProtocolException(
+                "URL: " + uri + " returned unexpected response status code: " + status );
           }
         }
       };
 
       log.debug( "HTTP request line: {}", httpPost.getRequestLine() );
 
-      log.info( "Invoking target URL: {}", url );
+      log.info( "Invoking target URL: {}", uri );
       String responseText = httpClient.execute( httpPost, responseHandler );
 
       log.debug( "Response text: {}", responseText );
 
       if ( !responseText.trim().isEmpty() )
       {
-      /*
-       * We use the HTTP response text as the Quartz job execution result. This code can then be easily
-       * viewed in the Execution History in the QuartzDesk GUI and it can be, for example, used to trigger
-       * execution notifications.
-       */
+        /*
+         * We use the HTTP response text as the Quartz job execution result. This code can then be easily
+         * viewed in the Execution History in the QuartzDesk GUI and it can be, for example, used to trigger
+         * execution notifications.
+         */
         context.setResult( responseText );
       }
 
@@ -128,7 +185,7 @@ public class UrlInvokerJob
     }
     catch ( IOException e )
     {
-      throw new JobExecutionException( "Error invoking URL: " + url, e );
+      throw new JobExecutionException( "Error invoking URL: " + uri, e );
     }
     finally
     {
